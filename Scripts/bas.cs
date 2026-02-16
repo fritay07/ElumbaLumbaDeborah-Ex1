97ced4c9ce9de8daeef55a85563b8b82dcc97669
déborah <debobojojo@gmail.com>
2026-02-15 23:02:03 -0500

    Ajout d'un build Web

--- Scripts/bas.cs
+++ Scripts/bas.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class bas : MonoBehaviour
 {
+
+    // vitesse de déplacement vers le bas Y
     public float vitesseY = 0.05f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +19,8 @@ public class bas : MonoBehaviour
 
         if (transform.position.y < -5.0f)
         {
+
+        // vas en haut de l'écran, retourne a son endroit de départ
             transform.position = new Vector2(transform.position.x, 6.0f);
         }
     }
